3260d3a49ba3e70c1850258cbd3a8e36959eafd2
Jussi Kinnunen <jussikin@iki.fi>
2015-11-29 19:58:29 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ArduinoSpeedometer
-Simple speedometer. Arduino + tm1637 4 digit display + serial gps module
+Simple speedometer. Arduino + tm1637 4 digit display + serial gps module.
+
+## Manifest:
+1. Arduino Nano
+2. Cheap tm1637 led tube display unit
+3. Neoblox 6M serial gps module unit
+
+## Operation:
+When connected to usb power, Arduino will start receiwing data from GPS unit. Soon as device gets gps fix it will start showing local time in finland. If device is moving faster than 10 kmh speed in kmh speed will be displayed instead of time.
+
+## Wiring:
+Gps and Display modules are wired for power and grond from nano. Singnaling into display is on pins (CLK12,and DIO11), gps module serial line is connected to pins 2,3.
---
+++ speedometer/speedometer.ino
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+#define CLK 12
+#define DIO 11
+TM1637Display display(CLK, DIO);
+SoftwareSerial mySerial(2, 3);
+TinyGPSPlus gps;
+
+void setup() { 
+  mySerial.begin(9600);
+  display.setBrightness(0x0f);
+}
+
+void loop() { // run over and over
+   while (mySerial.available() > 0)
+    if (gps.encode(mySerial.read())){
+      setDisplay();
+    }
+}
+
+void setDisplay(){
+  if(gps.speed.isValid()){
+    double speed = gps.speed.kmph();
+    if(speed>10){
+       display.showNumberDec(speed,false,4,0);
+       return;
+    }
+  }
+  if(gps.time.isValid()){
+    int minutes = gps.time.minute();   
+    display.showNumberDec(minutes,true,2,2);
+    int hours = gps.time.hour();
+    hours = hours+2;
+    if(hours>24)
+      hours=hours-24;
+    display.showNumberDec(hours,true,2,0);
+  }
+}
+
